20e7614904953a221e2a41da7711900b514ad56a
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-28 18:52:39 +0600

    insert data in realational table

--- src/app/api/demo/route.js
+++ src/app/api/demo/route.js
@@ -3,16 +3,28 @@ import { PrismaClient } from '/src/generated/prisma'
 
 export async function POST(req, res) {
     try{
-       const reqbody= await req.json();
-       const prisma=new PrismaClient();
-      let result= await prisma.Employee.create({
-           data:{
-               name:"Mohammad Ahasan3",
-               designation:"Engineer3",
-               city:"Chattogram3",
-               salary:12345
-           }
-       })
+        let prisma=new PrismaClient();
+        let result=await prisma.User.create({
+            data:{
+                email:"ahasan2@gmail.com",
+                password:"123456",
+                profile:{
+                    create:{
+                        firstName:"Mohammad2",
+                        lastName:"Ahasan2",
+                        mobile:"0170000000",
+                        city:"chattogram2",
+
+                    }
+                },
+                post:{
+                    create:{
+                        title:"new",
+                        description:"new post",
+                    }
+                }
+            }
+        })
        return NextResponse.json({status:200, data:result});
     }
     catch (e) {
